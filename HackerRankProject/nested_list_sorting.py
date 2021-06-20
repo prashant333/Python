6d0a9985f7c43bea66440ccb4ff30577e321d0e7
prashant333 <pmani157@gmail.com>
2021-06-20 21:31:20 +0530

    comments added

--- HackerRankProject/nested_list_sorting.py
+++ HackerRankProject/nested_list_sorting.py
@@ -1,7 +1,14 @@
+"""
+Given the names and grades for each student in a class of N students,
+store them in a nested list and print the name(s) of any student(s) having the second lowest grade.
+"""
+
 
 def sort_marksheet(nested_list):
 
     return sorted(nested_list, key= lambda x: x[1])
+
+
 marksheet = []
 for _ in range(0, int(input())):
     marksheet.append([input(), float(input())])
