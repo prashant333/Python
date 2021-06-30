f2ff4848b3901a6eb5a5a10590b07711f878bd4c
prashant333 <pmani157@gmail.com>
2021-06-30 11:59:02 +0530

    swap case program

---
+++ HackerRankProject/swapcase.py
@@ -0,0 +1,13 @@
+"""
+This program demonstrate swapping of case using python.
+"""
+
+
+def swap_case(s):
+    return s.swapcase()
+
+
+if __name__ == '__main__':
+    s = input()
+    result = swap_case(s)
+    print(result)
