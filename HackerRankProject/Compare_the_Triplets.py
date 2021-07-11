90180e9a0d9557c9cf4bcf6cb46a7edfbdfbdfb7
Prashant Mani <pmani157@gmail.com>
2021-07-11 12:13:54 +0530

    comparing the triplates

---
+++ HackerRankProject/Compare_the_Triplets.py
@@ -0,0 +1,35 @@
+#!/bin/python3
+
+#
+# Complete the 'compareTriplets' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts following parameters:
+#  1. INTEGER_ARRAY a
+#  2. INTEGER_ARRAY b
+#
+
+def compareTriplets(a, b):
+    # Write your code here
+    points = []
+    alice, bob = 0, 0
+    for i in range(3):
+        if a[i] > b[i]:
+            alice += 1
+        elif a[i] < b[i]:
+            bob += 1
+    points.append(alice)
+    points.append(bob)
+
+    return points
+
+
+if __name__ == '__main__':
+
+    a = list(map(int, input().rstrip().split()))
+
+    b = list(map(int, input().rstrip().split()))
+
+    result = compareTriplets(a, b)
+
+    print(result)
