b76a81ce6d1958701bcdce641897f498edd50513
prashant333 <pmani157@gmail.com>
2021-07-14 10:57:50 +0530

    unnecessary lines removed

--- HackerRankProject/Student_grading.py
+++ HackerRankProject/Student_grading.py
@@ -7,7 +7,7 @@
 def gradingStudent(grades):
     for i in range(len(grades)):
         if grades[i] >= 38:
-            if 5 - (grades[i] % 5 )< 3:
+            if 5 - (grades[i] % 5) < 3:
                 grades[i] = grades[i] + (5 - grades[i] % 5)
     return grades
 
