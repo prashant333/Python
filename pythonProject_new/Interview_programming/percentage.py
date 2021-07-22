12cdccbe9f0cf791b9b2b79fd3cfba61320149f1
Prashant Mani <pmani157@gmail.com>
2021-07-22 10:39:39 +0530

    comments added

--- pythonProject_new/Interview_programming/percentage.py
+++ pythonProject_new/Interview_programming/percentage.py
@@ -3,12 +3,13 @@
 n = int(input("Enter total number of values:"))
 student_list = {}
 for _ in range(n):
-    name, *lines = input("Enter values:").split() # *lines, here * is used to store the rest of input in variable named lines.
+    # *lines, here * is used to store the rest of input in variable named lines.
+    name, *lines = input("Enter values:").split()
     score = list(map(float, lines))
     student_list[name] = score
 
 query_name = input("Enter student name: ")
 total_marks = sum(student_list[query_name])
 average = total_marks/3
-print("%.2f"%(average)) # here "%.2f"% is a format specifier.
+print("%.2f" % average)  # here "%.2f"% is a format specifier.
 
