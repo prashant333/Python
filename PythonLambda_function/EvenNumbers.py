31ca26e076c4484328ce1baf152ec1c46aaa893e
Prashant Mani <pmani157@gmail.com>
2021-07-23 11:53:41 +0530

    Lambda function implementation

---
+++ PythonLambda_function/EvenNumbers.py
@@ -0,0 +1,11 @@
+"""
+This set of code demonstrates the use of lambda function to obtain even numbers from a set of given list.
+"""
+
+
+def FindEven(given_list):
+    return list(filter(lambda x: x % 2 == 0, given_list))
+
+
+unsorted_list = list(map(int, input("Enter the list values to be sorted: ").split()))
+print(FindEven(unsorted_list))
