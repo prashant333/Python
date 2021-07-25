e24a5d94149347dbe31a7413582c257b09a1950b
Prashant Mani <pmani157@gmail.com>
2021-07-25 18:20:50 +0530

    comments updated

--- PythonLambda_function/characterAt.py
+++ PythonLambda_function/characterAt.py
@@ -1,6 +1,4 @@
-"""
-This program demonstrate the use of lambda function to find a given character at the beginning of a text.
-"""
+# This program demonstrate the use of lambda function to find a given character at the beginning of a text.
 
 starts_with = lambda x: True if x.startswith('P') else False
 print(starts_with('Python'))
