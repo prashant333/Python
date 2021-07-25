919d44c46e063a147a49c8043621b03fc5473a78
Prashant Mani <pmani157@gmail.com>
2021-07-25 18:52:58 +0530

    comments updated

--- PythonLambda_function/EvenNumbers.py
+++ PythonLambda_function/EvenNumbers.py
@@ -1,7 +1,4 @@
-"""
-This set of code demonstrates the use of lambda function to obtain even numbers from a set of given list.
-"""
-
+# This set of code demonstrates the use of lambda function to obtain even numbers from a set of given list.
 
 def FindEven(given_list):
     return list(filter(lambda x: x % 2 == 0, given_list))
