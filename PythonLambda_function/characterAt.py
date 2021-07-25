ddefec2c01ceadc5bbbdfa2e14a288378a8d2d96
prashant333 <pmani157@gmail.com>
2021-07-25 10:57:41 +0530

    finding character at the start of a string

---
+++ PythonLambda_function/characterAt.py
@@ -0,0 +1,9 @@
+"""
+This program demonstrate the use of lambda function to find a given character at the beginning of a text.
+"""
+
+starts_with = lambda x: True if x.startswith('P') else False
+print(starts_with('Python'))
+starts_with = lambda x: True if x.startswith('P') else False
+print(starts_with('Java'))
+
