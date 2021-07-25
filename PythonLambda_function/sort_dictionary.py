5aaea60c5dd7c07f01c55df63592d8748ddd2a14
Prashant Mani <pmani157@gmail.com>
2021-07-25 10:14:52 +0530

    sort dictionary using lambda function

---
+++ PythonLambda_function/sort_dictionary.py
@@ -0,0 +1,14 @@
+"""
+Using lambda function to sort a given dictionary.
+
+"""
+
+model_varient = [{'make': 'Nokia', 'model': 216, 'color': 'Black'}, {'make': 'Mi Max', 'model': '2', 'color': 'Gold'}, {'make': 'Samsung', 'model': 7, 'color': 'Blue'}]
+
+
+def sort_dictionary(dic):
+    sorted_model = sorted(dic, key=lambda x: x['color'])
+    return sorted_model
+
+
+print(sort_dictionary(model_varient))
