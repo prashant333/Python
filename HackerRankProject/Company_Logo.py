89fb5df6a8db111a2069d5cc6df5cde49c3e4e04
Prashant Mani <pmani157@gmail.com>
2021-07-31 22:27:23 +0530

    comments updated

--- HackerRankProject/Company_Logo.py
+++ HackerRankProject/Company_Logo.py
@@ -1,11 +1,10 @@
+
 """
 A newly opened multinational brand has decided to base their company logo on the three most common characters
 in the company name. They are now trying out various combinations of company names and logos based on this
 condition. Given a string s, which is the company name in lowercase letters, your task is to find the top three
 most common characters in the string.
 """
-
-
 if __name__ == '__main__':
     s = input("Enter company name: ")
 
