72deed9d98d581807644a6906c1399c6dd86bd4d
Prashant Mani <pmani157@gmail.com>
2021-08-01 12:53:09 +0530

    updated comments

--- pythonProject_new/Interview_programming/shoe_shop.py
+++ pythonProject_new/Interview_programming/shoe_shop.py
@@ -1,12 +1,16 @@
+# Hackerrank challenge.
 import collections
 
 numShoes = int(input())
+# using counter to count the different size of shoes available in shop
 shoes = collections.Counter(map(int, input().split()))
+# takes input for number of customers
 numCust = int(input())
 
 income = 0
 
 for i in range(numCust):
+    # taking input for size requirement by the customer and price of the shoe.
     size, price = map(int, input().split())
     if shoes[size]:
         income += price
