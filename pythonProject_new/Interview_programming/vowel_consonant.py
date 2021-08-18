d8b4e052b2a5e8806dcdf40cb802f572ca16cfed
Prashant Mani <pmani157@gmail.com>
2021-08-18 11:03:56 +0530

    finding vowel and consonant in a given string

---
+++ pythonProject_new/Interview_programming/vowel_consonant.py
@@ -0,0 +1,15 @@
+# separate the vowel and consonant from a given string.
+
+def vowelConsonant(s):
+    vow = []
+    con = []
+    for i in range(len(s)):
+        if (s[i] == 'A' or s[i] == 'a' or s[i] == 'E' or s[i] == 'e' or s[i] == 'I'
+                or s[i] == 'i' or s[i] == 'O' or s[i] == 'o' or s[i] == 'U' or s[i] == 'u'):
+            vow.append(s[i])
+        else:
+            con.append(s[i])
+    print(vow)
+    print(con)
+
+vowelConsonant("howareyou")
