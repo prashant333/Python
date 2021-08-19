77d2d52bd79ea9333d3d9a53f46ec54fc703a614
prashant333 <pmani157@gmail.com>
2021-08-19 17:09:06 +0530

    updated for loop to implement enumerate

--- pythonProject_new/Interview_programming/vowel_consonant.py
+++ pythonProject_new/Interview_programming/vowel_consonant.py
@@ -3,12 +3,12 @@
 def vowelConsonant(s):
     vow = []
     con = []
-    for i in range(len(s)):
-        if (s[i] == 'A' or s[i] == 'a' or s[i] == 'E' or s[i] == 'e' or s[i] == 'I'
-                or s[i] == 'i' or s[i] == 'O' or s[i] == 'o' or s[i] == 'U' or s[i] == 'u'):
-            vow.append(s[i])
+    for i in enumerate(s):
+        if (i[1] == 'A' or i[1] == 'a' or i[1] == 'E' or i[1] == 'e' or i[1] == 'I'
+                or i[1] == 'i' or i[1] == 'O' or i[1] == 'o' or i[1] == 'U' or i[1] == 'u'):
+            vow.append(i[1])
         else:
-            con.append(s[i])
+            con.append(i[1])
     print(vow)
     print(con)
 
