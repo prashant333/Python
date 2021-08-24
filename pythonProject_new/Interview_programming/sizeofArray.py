c459c3911581405bdd9a696248024a0f267220a9
prashant333 <pmani157@gmail.com>
2021-08-24 14:17:36 +0530

    shrinking of list size.

--- pythonProject_new/Interview_programming/sizeofArray.py
+++ pythonProject_new/Interview_programming/sizeofArray.py
@@ -7,3 +7,10 @@ for i in range(10):
     b = sys.getsizeof(data)
     print("length: {0:3d}; size in bytes: {1:4d}".format(a, b))
     data.append(None)
+print("********************************")
+# below lines of code shows how the list size shrinks when list item is popped.
+for i in range(10, 0, -1):
+    a = len(data)
+    b = sys.getsizeof(data)
+    print("length: {0:3d}; size in bytes: {1:4d}".format(a, b))
+    data.pop()
