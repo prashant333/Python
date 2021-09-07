1a32b7e997b38014598940576be25c8a3b79d594
prashant333 <pmani157@gmail.com>
2021-09-07 11:38:06 +0530

    Rangoli design code

---
+++ HackerRankProject/rangoli.py
@@ -0,0 +1,18 @@
+import string
+
+
+def print_rangoli(size):
+    # your code goes here
+    alpha = string.ascii_lowercase
+
+    n = size
+    L = []
+    for i in range(n):
+        s = "-".join(alpha[i:n])
+        L.append((s[::-1] + s[1:]).center(4 * n - 3, "-"))
+    print('\n'.join(L[:0:-1] + L))
+
+
+if __name__ == '__main__':
+    n = int(input("Enter the width length: "))
+    print_rangoli(n)
