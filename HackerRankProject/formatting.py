3484db98f4842c7b36d82c6e69e9058e120f0804
prashant333 <pmani157@gmail.com>
2021-09-07 14:24:30 +0530

    comments updated

--- HackerRankProject/formatting.py
+++ HackerRankProject/formatting.py
@@ -1,9 +1,9 @@
+# String formatting.
 def print_formatted(number):
-    # your code goes here
     n = number
     width = len("{0:b}".format(n))
-    for i in range(1,n+1):
-      print("{0:{width}d} {0:{width}o} {0:{width}X} {0:{width}b}".format(i, width=width))
+    for i in range(1, n+1):
+        print("{0:{width}d} {0:{width}o} {0:{width}X} {0:{width}b}".format(i, width=width))
 
 
 if __name__ == '__main__':
