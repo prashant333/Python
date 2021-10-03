8ea1c7bbffcfadc4ee75cb9642087307670c92be
prashant333 <pmani157@gmail.com>
2021-10-03 19:41:18 +0530

    formatted properly

--- HackerRankProject/ginortS.py
+++ HackerRankProject/ginortS.py
@@ -4,6 +4,6 @@ for i in sorted(input()):
     if i.isalpha():
         x = u if i.isupper() else l
     else:
-        x = o if int(i)%2 else e
+        x = o if int(i) % 2 else e
     x.append(i)
 print("".join(l+u+o+e))
