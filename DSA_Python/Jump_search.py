85789554ec6995fc4ac7225f34b5dd2d0a304a16
prashant333 <pmani157@gmail.com>
2021-12-07 12:01:00 +0530

    algorithm analysis added for jump search

--- DSA_Python/Jump_search.py
+++ DSA_Python/Jump_search.py
@@ -36,3 +36,14 @@ l = len(a)
 result = jump_search(a, 233, l)
 print("Element is present at index " + str(result))
 
+"""
+Let's do a more general analysis of how Jump Search performs. We'll once again consider the worst-case scenario 
+where the element to be found is at the end of the list.
+
+For a list of n elements and a block size of m, Jump Search would ideally perform n/m jumps. Considering the 
+block size to be as √n, the runtime too would be O(√n).
+
+This puts Jump Search in between Linear Search (worst) with a runtime complexity of O(n) and Binary Search (best) 
+with a runtime complexity of O(log n). Hence, Jump Search can be used in places where the binary search is not 
+feasible and linear search is too costly.
+"""
