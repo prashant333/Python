d779ea6108f60cfbf8d4606f097609a8bbc11c65
prashant333 <pmani157@gmail.com>
2021-12-11 19:54:14 +0530

    comments updated for bubble sort.

--- DSA_Python/bubble_sort.py
+++ DSA_Python/bubble_sort.py
@@ -5,11 +5,14 @@ Bubble sort works by repeatedly swapping the two elements if they are in wrong o
 
 
 def bubble_sort(a):
+    # this is used to traverse the entire list
     for i in range(len(a)):
+        # here we decrement the count by i and 1, since the last elements are already sorted form the last loop.
         for j in range(0, (len(a)-i-1)):
+            # comparing the two concurrent list items.
             if a[j] > a[j+1]:
+                # swapping the two elements if they in wrong order.
                 a[j], a[j+1] = a[j+1], a[j]
-
     return a
 
 
