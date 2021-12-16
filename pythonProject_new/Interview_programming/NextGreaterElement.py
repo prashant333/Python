f52801f352dc45cc662f4d661e1dc207bbc6e05d
prashant333 <pmani157@gmail.com>
2021-12-16 20:02:41 +0530

    finding the next greater value

---
+++ pythonProject_new/Interview_programming/NextGreaterElement.py
@@ -0,0 +1,19 @@
+"""
+Given an array, print the Next Greater Element (NGE) for every element. The Next greater Element for an element x
+is the first greater element on the right side of x in the array. Elements for which no greater element exist,
+consider the next greater element as -1.
+"""
+
+
+def nextGreaterElement(arr):
+    for i in range(len(arr)):
+        next = -1
+        for j in range(i+1, len(arr)):
+            if arr[i] < arr[j]:
+                next = arr[j]
+                break
+        print("Next Greater Element for {} is {}".format(arr[i], next))
+
+
+x = [13, 7, 6, 12]
+nextGreaterElement(x)
