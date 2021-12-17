29f6b59f6fdfb0256ed2b37fe3548fbeabc5de10
prashant333 <pmani157@gmail.com>
2021-12-17 12:18:58 +0530

    queue data structure

---
+++ DSA_Python/queue.py
@@ -0,0 +1,38 @@
+# implementing queue data structure in python
+
+class Queue:
+
+    def __init__(self):
+        self.queue = []
+
+    def enqueue(self, item):
+        self.queue.append(item)
+
+    def dequeue(self):
+        if len(self.queue) < 1:
+            return None
+        else:
+            # pop(0) ensures that the first element is removed
+            # when dequeue method is called.
+            self.queue.pop(0)
+
+    def display(self):
+        print(self.queue)
+
+    def size(self):
+        return len(self.queue)
+
+
+q = Queue()
+q.enqueue(1)
+q.enqueue(2)
+q.enqueue(3)
+q.enqueue(4)
+q.enqueue(5)
+
+q.display()
+
+q.dequeue()
+
+print("After removing an element")
+q.display()
