4088fdecb5c33e31113e5964f6cd669d6f4c0f71
prashant333 <pmani157@gmail.com>
2021-12-19 22:49:08 +0530

    comments updated

--- python_Class/basic_class.py
+++ python_Class/basic_class.py
@@ -1,8 +1,14 @@
 # Python class declaration
 
 class Person:
+
+    #  __init__ is used as a constructor to initialize the object of this class
+
     def __init__(self, name):
         self.name = name
+
+# here Person in the parenthesis represents how Inheritance works in python.
+# class MDPerson inherits the Person class
 
 
 class MDPerson(Person):
@@ -14,11 +20,13 @@ class JDPerson(Person):
     def __init__(self, name):
         self.name = name + " Esquire"
 
+# person, doctor, lawyer are the instances of the respective class
+
 
 person = Person('Fudd')
 doctor = MDPerson('Fudd')
 lawyer = JDPerson('Fudd')
-
+#  here we access the specific attribute "name" of the person instance
 print(person.name)
 print(doctor.name)
 print(lawyer.name)
