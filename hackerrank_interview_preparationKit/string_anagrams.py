89fa017492605db230a18af515568aacdc18e686
prashant333 <pmani157@gmail.com>
2021-12-24 10:37:17 +0530

    comments updated

--- hackerrank_interview_preparationKit/string_anagrams.py
+++ hackerrank_interview_preparationKit/string_anagrams.py
@@ -1,14 +1,11 @@
 #!/bin/python3
 
 from collections import Counter
-#
 # Complete the 'makeAnagram' function below.
-#
 # The function is expected to return an INTEGER.
 # The function accepts following parameters:
 #  1. STRING a
 #  2. STRING b
-#
 
 
 def makeAnagram(a, b):
