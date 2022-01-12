e6cb61a1c0039ab1ad3aa2130346d6decd5cf207
prashant333 <pmani157@gmail.com>
2022-01-12 13:53:07 +0530

    comment updated

--- hackerrank_interview_preparationKit/tweetCount.py
+++ hackerrank_interview_preparationKit/tweetCount.py
@@ -3,19 +3,23 @@
 from collections import Counter
 
 n = int(input())
-tweet_record = []
+tweet_record = []            # a list variable to store the tweets.
 for i in range(n):
     total_record = int(input())
     for j in range(total_record):
-        tweet_record.append(input())
+        tweet_record.append(input())      # this is where the tweets will be appended in the list.
 
+# we use list comprehension and split to take out the names of person tweeting.
 name = [val.split()[0] for val in tweet_record]
+
+# we use counter to count the total number of occurrence of person names
 name_count = Counter(name)
-x = name_count.most_common()[0]
-for i in x:
+x = name_count.most_common()[0]   # we take out the first name from the output in x
+for i in x:                     # x is tuple and we use for loop to print out the output.
     print(i, end=" ")
 
 """
+The code above only meets the below output. working on other test cases.
 Sample 1:
 Input 
 1
