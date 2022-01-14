5ee27b22d76a1010b8e19dc133c8618649016843
prashant333 <pmani157@gmail.com>
2022-01-14 09:56:17 +0530

    retrieve dictionary key by value.

--- hackerrank_interview_preparationKit/Caption_room.py
+++ hackerrank_interview_preparationKit/Caption_room.py
@@ -7,7 +7,11 @@ rooms = list(map(int, input().split()))
 # We use the counter to count total number of rooms entries.
 rooms_1 = Counter(rooms)
 
+print(list(rooms_1.keys())[list(rooms_1.values()).index(1)])
+
 # Room entry that is exactly one is the caption's room.
+"""
 for x, y in rooms_1.items():
     if y == 1:
         print("Caption's room is room number: ", x)
+"""
