826bba92d31bc2d98ebde154a2099ab22d101c25
prashant333 <pmani157@gmail.com>
2022-01-31 09:25:34 +0530

    f string updated

--- pythonProject_mini/countdownTimmer.py
+++ pythonProject_mini/countdownTimmer.py
@@ -5,7 +5,8 @@ import time
 def countdown(t):
     while t:
         minute, sec = divmod(t, 60)
-        timer = '{:02d}:{:02d}'.format(minute, sec)
+        # timer = '{:02d}:{:02d}'.format(minute, sec)
+        timer = f'{minute:02d}:{sec:02d}'
         print(timer, end="\r")
         time.sleep(1)
         t -= 1
