a7f7331b75ab4f2863b16ea6eea0db5c75c40a74
prashant333 <pmani157@gmail.com>
2022-01-31 22:18:40 +0530

    code updated

--- pythonProject_mini/profilePicGitHub.py
+++ pythonProject_mini/profilePicGitHub.py
@@ -4,8 +4,13 @@ from bs4 import BeautifulSoup as bs
 
 github_user = input("Enter the github user name: ")
 url = "https://github.com/" + github_user
-r = requests.get(url)
+r = requests.get(url).text
+soup = bs(r, 'html.parser')
+profile_pic = soup.find('img', class_='avatar avatar-user width-full border color-bg-default')['src']
+print(profile_pic)
+
+"""
 soup = bs(r.content, 'html.parser')
 profile_img = soup.find('img', {'alt': 'Avatar'})['src']
 print(profile_img)
-
+"""
