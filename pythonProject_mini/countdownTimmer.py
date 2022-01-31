d39fb142f86a1edd812466a6143a45688b274a75
prashant333 <pmani157@gmail.com>
2022-01-31 09:29:46 +0530

    comments added

--- pythonProject_mini/countdownTimmer.py
+++ pythonProject_mini/countdownTimmer.py
@@ -4,12 +4,12 @@ import time
 
 def countdown(t):
     while t:
-        minute, sec = divmod(t, 60)
+        minute, sec = divmod(t, 60)  # used to convert given input into minutes and second
         # timer = '{:02d}:{:02d}'.format(minute, sec)
         timer = f'{minute:02d}:{sec:02d}'
-        print(timer, end="\r")
-        time.sleep(1)
-        t -= 1
+        print(timer, end="\r")  # print the counter
+        time.sleep(1)   # while loop will run every 1 sec, hence printing the counter with decreasing sec count.
+        t -= 1          # reduce the time, i,e sec by 1
     print("Countdown completed!")
 
 
