3a3e44d811fc1337eeddbfef4ef160b76c248f76
prashant333 <pmani157@gmail.com>
2022-02-03 12:04:16 +0530

    code updated, comments added

--- pythonProject_new/Interview_programming/firstLast.py
+++ pythonProject_new/Interview_programming/firstLast.py
@@ -2,20 +2,18 @@
  if target is not present in the given list, return [-1, -1]
 """
 
-result = []
-
 
 def firstLast(arr, t):
 
     for i in range(len(arr)):
-        if arr[i] == t:
-            result.append(i)
-            while i+1 < len(arr) and arr[i+1] == t:
+        if arr[i] == t:     # this is the first occurrence of the target
+            start = i
+            while i+1 < len(arr) and arr[i+1] == t:    # we starts to search for the last position of the target.
                 i += 1
-            result.append(i)
-            break
-    return result
+            return [start, i]      # return after the while loops is completed.
+
+    return [-1, -1]   # return [-1,-1] if target not found
 
 
 z = [1,1,2,4,5,5,5,5,5,7,7,7,7,7,7,9]
-print(firstLast(z, 7))
+print(firstLast(z, 10))
