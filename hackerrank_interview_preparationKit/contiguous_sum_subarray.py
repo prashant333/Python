7fa2cecf7067a5fbfe8d5f53cae3850ca8dd47d1
prashant <pmani157@gmail.com>
2023-03-17 15:17:27 +0530

    largest contiguous sub array sum

---
+++ hackerrank_interview_preparationKit/contiguous_sum_subarray.py
@@ -0,0 +1,19 @@
+# Find the contiguous sub array within a 1D array with higest sum.
+
+def findSum(l):
+    x =[[]]
+    maxi = []
+    for i in range(len(l)+1):
+        for j in range(i):
+            x.append(l[j:i])
+    # finding the sum of subarray 
+    for i in x:
+        maxi.append(sum(i))
+    print("Largest sum of subarray is: ",max(maxi))
+    for i in x:
+        if sum(i) == max(maxi):
+            print("Largest contiguous subarray is: ",i)
+
+
+l1 = [-2, -3, 4, -1, -2, 1, 5, -3]
+findSum(l1)
