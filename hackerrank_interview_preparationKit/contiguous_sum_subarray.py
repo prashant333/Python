e2f58273d088b5b41f5dbc7f1964f10a42cea203
prashant <pmani157@gmail.com>
2023-03-17 15:57:31 +0530

    comments added

--- hackerrank_interview_preparationKit/contiguous_sum_subarray.py
+++ hackerrank_interview_preparationKit/contiguous_sum_subarray.py
@@ -3,13 +3,18 @@
 def findSum(l):
     x =[[]]
     maxi = []
+
+    # we will create all possible contiguous subarray using the loops below
     for i in range(len(l)+1):
         for j in range(i):
             x.append(l[j:i])
-    # finding the sum of subarray 
+
+    # finding the sum of all the subarray 
     for i in x:
         maxi.append(sum(i))
     print("Largest sum of subarray is: ",max(maxi))
+
+    # we will then find which subarray sum is equal to the max sum found 
     for i in x:
         if sum(i) == max(maxi):
             print("Largest contiguous subarray is: ",i)
